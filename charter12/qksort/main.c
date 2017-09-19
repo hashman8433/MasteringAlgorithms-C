9ad45d4a926caeade0c6a809676dd0b4fe5e0151
hashman8433 <843355856@qq.com>
2017-09-20 00:24:07 +0800

    Update main.c

--- charter12/qksort/main.c
+++ charter12/qksort/main.c
@@ -33,14 +33,14 @@ int issort(void *data, int size, int esize,int (*compare)(const void *key1,const
 		memcpy(key, &a[j * esize], esize);	
 		i = j - 1;
 		
-		while(i > 0 && compare( &a[i * esize], &a[j * esize]) > 0)
+		while(i > 0 && compare( &a[i * esize], key) > 0)
 		{
 			memcpy(&a[(i + 1) * esize], &a[i * esize], esize);
 			i--;
 		}
-		memcpy( &a[i * esize], key, esize);
+		memcpy( &a[(i + 1) * esize], key, esize);
 	}
-	return 1;
+	return 0;
 
 }
 
@@ -70,22 +70,22 @@ static int partition(void *data, int esize, int i, int k, int (*compare)
 	r[1] = (rand() % (k - i + 1) + i);
 	r[2] = (rand() % (k - i + 1) + i);
 		
-	if(issort(a,3,sizeof(int),compare_int) < 0 )
+	if(issort(r,3,sizeof(int),compare_int) < 0 )
 		return -1;	
 	memcpy( pval, &a[r[1] * esize], esize);	
 
-	printf("issort : %d %d %d", r[0], r[1], r[2]);
+	printf("issort : %d %d %d  pval : %d\n ", r[0], r[1], r[2], *(int *)pval);
 	i--;
 	k++;
 	while(1)
 	{
 		do {
 			k--;	
-		}while (compare(&a[k * esize], &pval));
+		}while (compare(&a[k * esize], pval) > 0);
 
 		do{
 			i++;
-		}while( compare(&a[i * esize], &pval));	
+		}while( compare(&a[i * esize], pval) < 0);	
 	
 		if(k <= i){
 			break;
@@ -95,7 +95,7 @@ static int partition(void *data, int esize, int i, int k, int (*compare)
 			memcpy( &a[k * esize], temp, esize);
 		}
 	}
-
+	
 	
 	free(pval);
 	free(temp);
@@ -106,8 +106,11 @@ int qksort(void *data, int size, int esize, int i, int k, int (*compare)(const v
 {
 
 	int j = 0;
+	printf("i = %d k = %d\n" ,i, k);
+	if(k <= i) return -1;
 	
-	if((k = partition(data, esize, i, k, compare)) < 0) return -1;
+	if((j = partition(data, esize, i, k, compare)) < 0) return -1;
+	printf("j = %d\n", j);
 	
 	if(qksort(data,size, sizeof(int), i, j, compare_int) < 0)
 		return -1;
@@ -122,11 +125,13 @@ int qksort(void *data, int size, int esize, int i, int k, int (*compare)(const v
 int main()
 {
 
-	int qarray[10] = {0,1,2,5,8};
+	int qarray[10] = {0,1,7,5,4};
 
+	printf("%d %d %d %d %d\n",qarray[0], qarray[1], qarray[2], qarray[3], qarray[4]);
+	
 	qksort(qarray, 5, sizeof(int), 0, 4, compare_int);
 
 	//issort(qarray, 5, sizeof(int),compare_int);
-	printf("%d %d %d %d %d",qarray[0], qarray[1], qarray[2], qarray[3], qarray[4]);
+	printf("%d %d %d %d %d\n",qarray[0], qarray[1], qarray[2], qarray[3], qarray[4]);
 	return 1;
 }
